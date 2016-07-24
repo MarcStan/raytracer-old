22700effd7b33940d72cf632d99f7258f31c9a71
MarcStan <marcstan@live.com>
2016-07-24 15:08:48 +0200

    raytracer will fill color[] and spritebatch will draw it

---
+++ Raytracer/Raytracer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Raytracer
+{
+	public class Raytracer
+	{
+		/// <summary>
+		/// When called will trace the scene from the given camera location.
+		/// Width * height must equal the length of the color array.
+		/// Each pixel in the color array will be traced. The convention is X lines first then y lines (accessed via tracingTarget[x + y * width]).
+		/// </summary>
+		/// <param name="scene"></param>
+		/// <param name="camera"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <param name="tracingTarget"></param>
+		public void TraceScene(Scene scene, Camera camera, int width, int height, ref Color[] tracingTarget)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					tracingTarget[x + y * width] = Color.White;
+				}
+			}
+		}
+	}
+}
--- Raytracer/RaytracerGame.cs
+++ Raytracer/RaytracerGame.cs
@@ -1,12 +1,75 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace Raytracer
 {
 	public class RaytracerGame : Game
 	{
+		private readonly int _width = 800, _height = 600;
+		private Texture2D _raytracedScene;
+		private Color[] _pixels;
+		private readonly Raytracer _raytracer;
+		private Scene _scene;
+		private Camera _camera;
+		private SpriteBatch _spriteBatch;
+		private bool _sceneChanged;
+		private Stopwatch _watch;
+
 		public RaytracerGame()
 		{
-			new GraphicsDeviceManager(this);
+			new GraphicsDeviceManager(this)
+			{
+				PreferredBackBufferWidth = _width,
+				PreferredBackBufferHeight = _height
+			};
+			_raytracer = new Raytracer();
+			_pixels = new Color[_width * _height];
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+
+			SetupScene();
+
+			_raytracedScene = new Texture2D(GraphicsDevice, _width, _height);
+
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			_sceneChanged = true;
+			_watch = new Stopwatch();
+		}
+
+		private void SetupScene()
+		{
+			_scene = new Scene();
+			_camera = new Camera();
+		}
+
+		private void RaytraceScene()
+		{
+			_raytracer.TraceScene(_scene, _camera, _width, _height, ref _pixels);
+			_raytracedScene.SetData(_pixels);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				Exit();
+			}
+
+			if (_sceneChanged)
+			{
+				_sceneChanged = false;
+				_watch.Restart();
+				RaytraceScene();
+				_watch.Stop();
+				Window.Title = $"Raytracer - Last trace took {_watch.ElapsedMilliseconds}ms.";
+			}
 		}
 
 		protected override void Draw(GameTime gameTime)
@@ -14,6 +77,10 @@ namespace Raytracer
 			base.Draw(gameTime);
 
 			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			_spriteBatch.Begin();
+			_spriteBatch.Draw(_raytracedScene, Vector2.Zero, Color.White);
+			_spriteBatch.End();
 		}
 	}
 }
