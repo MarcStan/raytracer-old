25d9addb6486fa31b491beed7cd30db762ac9f33
MarcStan <marcstan@live.com>
2016-08-05 15:11:31 +0200

    added comment to build script as most PCs don't have msbuild in the path

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,5 @@
-msbuild Raytracer.sln /t:Build /p:Configuration=Release /nr:false
+REM If msbuild is not in your path, add it or use full path such as C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe
+msbuild.exe Raytracer.sln /t:Build /p:Configuration=Release /nr:false
 mkdir "!Releases"
 
 echo f|xcopy Raytracer\bin\Release\Raytracer.exe !Releases\Raytracer.exe /y
